b8200d84b55bff2790a86f0fb6f5ddcd12fafcfc
798351571 <l15639965018@163.com>
2024-02-26 18:22:07 +0800

    test1

---
+++ bus.java
@@ -0,0 +1,11 @@
+public class bus {
+    public static void main(String[] args) {
+		int count = 0;
+		count++;
+		count = count + 2 - 1;
+		count = count + 2 - 1;
+		count--;
+		count++;
+		System.out.println(count);
+	}
+}
